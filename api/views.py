94b0514f7da707539a15bba189f142c5738ecc44
shazy89 <erdo.shazy123@gmail.com>
2020-12-12 10:46:13 -0500

    react ready

--- api/views.py
+++ api/views.py
@@ -53,4 +53,11 @@ def taskUpdate(request, pk):
 
 	return Response(serializer.data)
 
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+	task = Task.objects.get(id=pk)
+	task.delete()
+
+	return Response('Item succsesfully delete!')
+
 
